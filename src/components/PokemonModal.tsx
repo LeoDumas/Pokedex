0d3c8fdac25dbd07018a4e21e918d934833001ec
LeoDumas <leo.ndumas980@gmail.com>
2024-01-20 16:41:00 +0100

    refactor: pokemon modal

--- src/components/PokemonModal.tsx
+++ src/components/PokemonModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Pokemon } from '../App';
 import { pokeIdFormatter } from '../tools/PokeIdFormatter';
 import PokeTypeCard from './PokeTypeCard';
@@ -11,10 +11,19 @@ interface PokemonModalProps {
 }
 
 const PokemonModal: React.FC<PokemonModalProps> = ({ pokemon, onClose }) => {
+   const hasPreEvolution = !!pokemon.apiPreEvolution;
+    useEffect(()=> {
+      console.log(pokemon.apiPreEvolution);
+      console.log(pokemon.apiEvolutions);
+      console.log(hasPreEvolution);
+ 
+    })
+
     return (
         // ToDo : change bg-gray on conditional render because when click on modal, it close it self
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-80" onClick={onClose}>
-            <div className=" flex bg-white p-4 ">
+        <div className=" fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex items-center justify-center">
+            <div className='absolute z-10 w-screen h-screen bg-black opacity-70' onClick={onClose}></div>
+              <div className=" flex z-40 bg-white p-4 ">
                 {/* Left part */}
                 <div className='w-1/2'>
                     {/* ToDo: create picture component where we can add multiple picture dans buttons to switch between those */}
@@ -42,24 +51,26 @@ const PokemonModal: React.FC<PokemonModalProps> = ({ pokemon, onClose }) => {
 
                     {/* ToDo : For Weakness and resistance, check for damage_relation in apiResistances */}
                     <h2 className='mt-5 text-xl font-semibold'>Weakness</h2>
-                    <ul className="flex flex-wrap justify-start mt-1">
-                        {pokemon.apiResistances
-                            .filter(type => type.damage_multiplier < 1)
-                            .map((type, index) => (
+                    <ul className="flex flex-wrap justify-start mt-1 max-w-xl">
+                        {pokemon.apiResistances 
+                            .filter(type => type.damage_multiplier < 1) 
+                            .map((type, index) => ( 
                                 <PokeTypeCard2 name={type.name} damage_multiplier={type.damage_multiplier} damage_relation={type.damage_relation} keyIndex={index} />
-                            ))}
-                    </ul>
-
-                    {/* Ressistance */}
+                            ))} 
+                    </ul> {/* Ressistance */} 
+                    
                     <h2 className='mt-5 text-xl font-semibold'>Weakness</h2>
-                    <ul className="flex flex-wrap justify-start mt-1">
+                    <ul className="flex flex-wrap justify-start mt-1 max-w-xl">
                         {pokemon.apiResistances
                             .filter(type => type.damage_multiplier > 1)
                             .map((type, index) => (
                                 <PokeTypeCard2 name={type.name} damage_multiplier={type.damage_multiplier} damage_relation={type.damage_relation} keyIndex={index} />
                             ))}
                     </ul>
-                    
+          
+                    {(hasPreEvolution) && 
+                        <h2>Pre Evolution</h2>
+                    }
                 </div>
             </div>
         </div>
